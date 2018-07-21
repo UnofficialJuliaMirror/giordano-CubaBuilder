73286e5e6c92dff2ad62fa632436b41d3a9937bd
Mosè Giordano <mose@gnu.org>
2018-07-21 14:51:07 +0200

    Update the script and support a few more systems
    Support for FreeBSD is still missing.

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,11 +1,17 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-commit_sha = "1c3e17736216bb01f6a6d32746b851869a25c867"
+name = "Cuba"
+version = v"4.2a"
+commit_sha = "75a98808e0246a8b716df6478169711144f66e0c"
 
-# Collection of sources required to build CubaBuilder
+# Collection of sources required to build Cuba
+name = "Cuba"
+version = v"4.2"
 sources = [
     "https://github.com/giordano/cuba/archive/$commit_sha.tar.gz" =>
-    "e572dcb49c52bcdff5c20a95caff2fe0aae504c56200d798a00a960eb27f8a73",
+    "d5984ba8678acf4afba78a37c6e01a0fedc6d3ab6052eef196d1c9a5382dc869",
 
 ]
 
@@ -21,14 +27,18 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    BinaryProvider.Linux(:armv7l, :glibc, :eabihf),
-    BinaryProvider.Linux(:powerpc64le, :glibc, :blank_abi),
-    BinaryProvider.Linux(:x86_64, :glibc, :blank_abi),
-    BinaryProvider.Linux(:aarch64, :glibc, :blank_abi),
-    BinaryProvider.Windows(:x86_64, :blank_libc, :blank_abi),
-    BinaryProvider.Linux(:i686, :glibc, :blank_abi),
-    BinaryProvider.Windows(:i686, :blank_libc, :blank_abi),
-    BinaryProvider.MacOS(:x86_64, :blank_libc, :blank_abi)
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    MacOS(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
 ]
 
 # The products that we will ensure are always built
@@ -40,4 +50,4 @@ products(prefix) = [
 dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "Cuba", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
